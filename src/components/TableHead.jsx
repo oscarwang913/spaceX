414c8ac72f2a0d83a27f7d1fd09e116a3f64af71
DESKTOP-JM65FNB\oscar <garnett284@gmail.com>
2022-08-14 21:50:15 +0800

    add sort icon

--- src/components/TableHead.jsx
+++ src/components/TableHead.jsx
@@ -2,6 +2,8 @@ import {useState} from 'react'
 import styles from '../styles/TableHead.module.scss'
 import { useContext } from 'react'
 import dataContext from '../context/dataContext'
+import upArrow from '../assets/icons8-up-48.png'
+import downArrow from '../assets/icons8-down-48.png'
 
 const TableHead = ({handleSorting}) => {
   const {tableHeadTitle} = useContext(dataContext)
@@ -15,17 +17,18 @@ const TableHead = ({handleSorting}) => {
     setOrder(sortingOrder)
     handleSorting(label, sortingOrder)
   }
-  
+
   return (
     <thead>
       <tr>
         {
-          tableHeadTitle.map(({columnName, label}) => (
+          tableHeadTitle.map(({columnName, label, order}) => (
             <th className={styles.head} 
                 key={label}
                 onClick={() => onSortingChange(label)}
             >
-              {columnName}
+              {columnName} 
+              {!order ? null : order === 'asc' ? <img src={upArrow} /> : <img src={downArrow} />}
             </th>
           ))
         }
